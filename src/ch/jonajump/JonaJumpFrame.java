1345c223e95d9af59b20fd0e7f57d6b24e03f2ec
Damian Brunold <damian@imagic.ch>
2012-08-20 10:10:22 +0200

    Use double buffered rendering, reformat code using spaces instead of tabs.

--- src/ch/jonajump/JonaJumpFrame.java
+++ src/ch/jonajump/JonaJumpFrame.java
@@ -9,26 +9,26 @@ import javax.swing.JFrame;
 
 public class JonaJumpFrame extends JFrame {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private JonaJumpPanel panel;
-	
-	public JonaJumpFrame() throws IOException {
-		super("JonaJump");
-		setLayout(new BorderLayout());
-		add(panel = new JonaJumpPanel());
-		pack();
-		setLocationRelativeTo(null);
-		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		addWindowListener(new WindowAdapter() {
-			public void windowClosing(WindowEvent e) {
-				panel.stopped = true;
-			}
-		});
-	}
+    private JonaJumpPanel panel;
 
-	public static void main(String[] args) throws IOException {
-		new JonaJumpFrame().setVisible(true);
-	}
+    public JonaJumpFrame() throws IOException {
+        super("JonaJump");
+        setLayout(new BorderLayout());
+        add(panel = new JonaJumpPanel());
+        pack();
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                panel.stopped = true;
+            }
+        });
+    }
+
+    public static void main(String[] args) throws IOException {
+        new JonaJumpFrame().setVisible(true);
+    }
 
 }
--- src/ch/jonajump/JonaJumpPanel.java
+++ src/ch/jonajump/JonaJumpPanel.java
@@ -3,6 +3,7 @@ package ch.jonajump;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
@@ -13,207 +14,224 @@ import javax.imageio.ImageIO;
 
 public class JonaJumpPanel extends Component implements Runnable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private static final int SCREEN_WIDTH = 800;
-	private static final int SCREEN_HEIGHT = 600;
-	
-	private static final int JUMP_VELOCITY = 25;
-	private static final int ACCEL_Y = 100;
-	
-	public volatile boolean stopped = false;
-	
-	private int x = 0;
-	private int y = 360; // TODO
-	private int player_offset_x = -SCREEN_WIDTH / 2;
-	private int player_offset_y = 0;
-	private int player_accel_y = 0;
-	private int jump_time = 0;
-	private int running_time = 0;
-	
-	private boolean looking_right = true;
-	private boolean running = false;
-	private boolean jumping = false;
+    private static final int SCREEN_WIDTH = 800;
+    private static final int SCREEN_HEIGHT = 600;
 
-	private BufferedImage background;
-	private BufferedImage player_standing_right;
-	private BufferedImage player_standing_left;
-	private BufferedImage player_running_right;
-	private BufferedImage player_running_left;
-	private BufferedImage player_jumping_right;
-	private BufferedImage player_jumping_left;
-	private BufferedImage player;
+    private static final int JUMP_VELOCITY = 25;
+    private static final int ACCEL_Y = 100;
 
-	public JonaJumpPanel() throws IOException {
-		loadImages();
-		addKeyListener(new KeyAdapter() {
-			@Override
-			public void keyPressed(KeyEvent e) {
-				super.keyPressed(e);
-				if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-					looking_right = true;
-					running = true;
-				} else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-					looking_right = false;
-					running = true;
-				} else if (e.getKeyCode() == KeyEvent.VK_SPACE) {
-					jumping = true;
-					jump_time = 0;
-					player_accel_y = 1;
-				}
-			}
-			@Override
-			public void keyReleased(KeyEvent e) {
-				super.keyReleased(e);
-				if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-					running = false;
-				} else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-					running = false;
-				}
-			}
-		});
-		setFocusable(true);
-		requestFocus();
-		Thread thread = new Thread(this);
-		thread.start();
-	}
+    private static final int FRAME_INTERVAL = 20;
 
-	private void loadImages() throws IOException {
-		background = getImage("background");
-		player_standing_right = getImage("player_standing_right");
-		player_standing_left = getImage("player_standing_left");
-		player_running_right = getImage("player_running_right");
-		player_running_left = getImage("player_running_left");
-		player_jumping_right = player_running_right; // TODO 
-		player_jumping_left = player_running_left; // TODO 
-	}
-	
-	private boolean isJumping() {
-		return jumping; 
-	}
-	
-	private boolean isRunning() {
-		return running; 
-	}
-	
-	private boolean isStanding() {
-		return !running && !jumping; 
-	}
-	
-	private boolean isLeft() {
-		return !isRight(); 
-	}
-	
-	private boolean isRight() {
-		return looking_right; 
-	}
-	
-	private BufferedImage getImage(String name) throws IOException {
-		return ImageIO.read(new File(JonaJumpPanel.class.getResource("/resources/" + name + ".png").getFile().replace("%20", " ")));
-	}
+    public volatile boolean stopped = false;
 
-	public Dimension getPreferredSize() {
-		return new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT);
-	}
+    private int x = 0;
+    private int y = 360; // TODO
+    private int player_offset_x = -SCREEN_WIDTH / 2;
+    private int player_offset_y = 0;
+    private int player_accel_y = 0;
+    private int jump_time = 0;
+    private int running_time = 0;
 
-	@Override
-	public void paint(Graphics g) {
-		super.paint(g);
-		updateWorld(g);
-	}
+    private boolean looking_right = true;
+    private boolean running = false;
+    private boolean jumping = false;
 
-	public void run() {
-		while (!stopped) {
-			Graphics g = getGraphics();
-			if (g != null) {
-				updateWorld(g);
-				g.dispose();
-			}
-			try {
-				Thread.sleep(20);
-			} catch (InterruptedException e) {
-				// ignore
-			}
-		}
-	}
-	
-	private void updateWorld(Graphics g) {
-		updateState();
-		renderWorld(g);
-	}
+    private BufferedImage background;
+    private BufferedImage player_standing_right;
+    private BufferedImage player_standing_left;
+    private BufferedImage player_running_right;
+    private BufferedImage player_running_left;
+    private BufferedImage player_jumping_right;
+    private BufferedImage player_jumping_left;
+    private BufferedImage player;
 
-	private void updateState() {
-		updatePlayerPositionX();
-		updatePlayerPositionY();
-		setPlayerImage();
-	}
+    private Image buffer;
 
-	private void updatePlayerPositionX() {
-		if (running) {
-			if (looking_right) {
-				if (player_offset_x < 0) {
-					player_offset_x += 10;
-					if (player_offset_x > 0) {
-						player_offset_x = 0;
-					}
-				} else {
-					x += 10;
-					if (x > background.getWidth() - SCREEN_WIDTH) {
-						x = background.getWidth() - SCREEN_WIDTH;
-						player_offset_x += 10;
-						if (player_offset_x > SCREEN_WIDTH / 2 - player.getWidth()) {
-							player_offset_x = SCREEN_WIDTH / 2 - player.getWidth() ;
-						}
-					}
-				}
-			} else {
-				if (player_offset_x > 0) {
-					player_offset_x -= 10;
-					if (player_offset_x < 0) {
-						player_offset_x = 0;
-					}
-				} else {
-					x -= 10;
-					if (x < 0) {
-						x = 0;
-						player_offset_x -= 10;
-						if (player_offset_x < -SCREEN_WIDTH / 2) {
-							player_offset_x = -SCREEN_WIDTH / 2;
-						}
-					}
-				}
-			}
-		}
-	}
+    public JonaJumpPanel() throws IOException {
+        loadImages();
+        addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                super.keyPressed(e);
+                if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                    looking_right = true;
+                    running = true;
+                } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+                    looking_right = false;
+                    running = true;
+                } else if (e.getKeyCode() == KeyEvent.VK_SPACE && !jumping) {
+                    jumping = true;
+                    jump_time = 0;
+                    player_accel_y = 1;
+                }
+            }
 
-	private void updatePlayerPositionY() {
-		if (jumping) {
-			player_offset_y = JUMP_VELOCITY * jump_time - player_accel_y * jump_time * jump_time;
-			jump_time++;
-			if (player_offset_y < 0) {
-				player_offset_y = 0;
-				player_accel_y = 0;
-				jumping = false; 
-			}
-		}
-	}
+            @Override
+            public void keyReleased(KeyEvent e) {
+                super.keyReleased(e);
+                if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                    running = false;
+                } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+                    running = false;
+                }
+            }
+        });
+        setFocusable(true);
+        requestFocus();
+        Thread thread = new Thread(this);
+        thread.start();
+    }
 
-	private void setPlayerImage() {
-		if (running) {
-			if (looking_right) player = player_running_right;
-			else player = player_running_left;
-		} else if (jumping) {
-			if (looking_right) player = player_jumping_right;
-			else player = player_jumping_left;
-		} else {
-			if (looking_right) player = player_standing_right;
-			else player = player_standing_left;
-		}
-	}
+    private void loadImages() throws IOException {
+        background = getImage("background");
+        player_standing_right = getImage("player_standing_right");
+        player_standing_left = getImage("player_standing_left");
+        player_running_right = getImage("player_running_right");
+        player_running_left = getImage("player_running_left");
+        player_jumping_right = player_running_right; // TODO
+        player_jumping_left = player_running_left; // TODO
+    }
 
-	private void renderWorld(Graphics g) {
-		g.drawImage(background, 0, 0, 800, 600, x, 0, x + 800, 600, null);
-		g.drawImage(player, 400 + player_offset_x, y - player_offset_y, null);
-	}
+    private boolean isJumping() {
+        return jumping;
+    }
+
+    private boolean isRunning() {
+        return running;
+    }
+
+    private boolean isStanding() {
+        return !running && !jumping;
+    }
+
+    private boolean isLeft() {
+        return !isRight();
+    }
+
+    private boolean isRight() {
+        return looking_right;
+    }
+
+    private BufferedImage getImage(String name) throws IOException {
+        return ImageIO.read(new File(JonaJumpPanel.class.getResource("/resources/" + name + ".png").getFile().replace("%20", " ")));
+    }
+
+    public Dimension getPreferredSize() {
+        return new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        updateWorld(g);
+    }
+
+    public void run() {
+        while (!stopped) {
+            Graphics g = getGraphics();
+            if (g != null) {
+                updateWorld(g);
+                g.dispose();
+            }
+            try {
+                Thread.sleep(FRAME_INTERVAL);
+            } catch (InterruptedException e) {
+                // ignore
+            }
+        }
+    }
+
+    private void updateWorld(Graphics g) {
+        updateState();
+        renderWorld(g);
+    }
+
+    private void updateState() {
+        updatePlayerPositionX();
+        updatePlayerPositionY();
+        setPlayerImage();
+    }
+
+    private void updatePlayerPositionX() {
+        if (running) {
+            if (looking_right) {
+                if (player_offset_x < 0) {
+                    player_offset_x += 10;
+                    if (player_offset_x > 0) {
+                        player_offset_x = 0;
+                    }
+                } else {
+                    x += 10;
+                    if (x > background.getWidth() - SCREEN_WIDTH) {
+                        x = background.getWidth() - SCREEN_WIDTH;
+                        player_offset_x += 10;
+                        if (player_offset_x > SCREEN_WIDTH / 2 - player.getWidth()) {
+                            player_offset_x = SCREEN_WIDTH / 2 - player.getWidth();
+                        }
+                    }
+                }
+            } else {
+                if (player_offset_x > 0) {
+                    player_offset_x -= 10;
+                    if (player_offset_x < 0) {
+                        player_offset_x = 0;
+                    }
+                } else {
+                    x -= 10;
+                    if (x < 0) {
+                        x = 0;
+                        player_offset_x -= 10;
+                        if (player_offset_x < -SCREEN_WIDTH / 2) {
+                            player_offset_x = -SCREEN_WIDTH / 2;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void updatePlayerPositionY() {
+        if (jumping) {
+            player_offset_y = JUMP_VELOCITY * jump_time - player_accel_y * jump_time * jump_time;
+            jump_time++;
+            if (player_offset_y < 0) {
+                player_offset_y = 0;
+                player_accel_y = 0;
+                jumping = false;
+            }
+        }
+    }
+
+    private void setPlayerImage() {
+        if (running) {
+            if (looking_right)
+                player = player_running_right;
+            else
+                player = player_running_left;
+        } else if (jumping) {
+            if (looking_right)
+                player = player_jumping_right;
+            else
+                player = player_jumping_left;
+        } else {
+            if (looking_right)
+                player = player_standing_right;
+            else
+                player = player_standing_left;
+        }
+    }
+
+    private void renderWorld(Graphics g) {
+        if (buffer == null) {
+            buffer = createImage(SCREEN_WIDTH, SCREEN_HEIGHT);
+        }
+        Graphics buffer_g = buffer.getGraphics();
+        buffer_g.drawImage(background, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, x, 0, x + SCREEN_WIDTH, SCREEN_HEIGHT, null);
+        buffer_g.drawImage(player, SCREEN_WIDTH / 2 + player_offset_x, y - player_offset_y, null);
+        buffer_g.dispose();
+        g.drawImage(buffer, 0, 0, null);
+    }
 
 }
